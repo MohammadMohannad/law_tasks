c55efc1d849b74ac8ba69699d249835bd2eeb7bd
MohammadMohannad <mhmmdalansary@gmail.com>
2024-01-07 21:01:34 +0300

    first commit

---
+++ tasks/tasks/doctype/tasks_management/tasks_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Mohammad Muhannad and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Tasks Management", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ tasks/tasks/doctype/tasks_management/test_tasks_management.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Mohammad Muhannad and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTasksManagement(FrappeTestCase):
+	pass
